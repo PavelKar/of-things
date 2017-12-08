49c7a28cfe81c87b85b9defff432aba84787e782
Simon Neutert <simonneutert@Simons-MBP.fritz.box>
2017-12-08 08:21:32 +0100

    jslint :)

--- js/hash-params.js
+++ js/hash-params.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   if (hashParams != "") {
     for(var i = 0; i < hashParams.length; i++){
       var p = hashParams[i].split('=');
-      document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
-    }
+      document.getElementById(p[0]).value = decodeURIComponent(p[1]);
+    };
   }
-})
+});
